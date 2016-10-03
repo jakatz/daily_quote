8874809cb9d54b386892e8c13f5fa77b72de243a
Jonathan Katz <jonathan.a.katz@gmail.com>
2016-10-03 15:47:20 -0700

    add quote component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import App from './components/App';
+import './style.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
