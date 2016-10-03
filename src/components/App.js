0eb5f7e7f749f1c08b329ec4f830747c952dd229
Jonathan Katz <jonathan.a.katz@gmail.com>
2016-10-03 15:47:42 -0700

    rename css to style.css

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Quote from './Quote';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Quote />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Quote extends Component {
+  render() {
+    return (
+      <div>Here is the quote component</div>
+    );
+  }
+}
+
+export default Quote;
