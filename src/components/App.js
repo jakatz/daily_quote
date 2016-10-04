9d3e4e5f223ac8729fd07f2d82a06911af49b76e
Jonathan Katz <jonathan.a.katz@gmail.com>
2016-10-03 20:43:54 -0700

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,51 @@ import React, { Component } from 'react';
 import Quote from './Quote';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    // bindings
+    this.getQuote = this.getQuote.bind(this);
+
+    // getInitialState
+    this.state = {
+      quote: "",
+      author: ""
+    };
+  }
+
+  setBackground(url) {
+    const page = document.querySelector('.daily-quote');
+    console.log(page);
+  }
+
+  getQuote(e) {
+    e.preventDefault();
+    fetch('http://quotes.rest/qod.json?category=inspire', {
+      method: 'get'
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      const quote = data.contents.quotes[0].quote;
+      const author = data.contents.quotes[0].author;
+
+      this.setBackground(data.contents.quotes[0].background);
+      this.setState({
+        quote: quote,
+        author: author
+      });
+    }).catch((err) => {
+      console.error("Error: ", err);
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <Quote />
+      <div className="daily-quote">
+        <Quote getQuote={this.getQuote} />
+        <p>{this.state.quote}</p>
+        <h1>{this.state.author}</h1>
       </div>
     );
   }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class Quote extends Component {
+  // componentDidMount() {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open('GET', this.props.source);
+  //   xhr.send(null);
+
+  //   xhr.onreadystatechange = ()=> {
+  //     if (xhr.readyState === 4) {
+  //       console.log(xhr.responseText);
+  //     } else {
+  //       console.log('Error: ', xhr.status);
+  //     }
+  //   }
+  // }
+
   render() {
+    const getQuoteButton = <button onClick={this.props.getQuote}>Get Quote</button>;
+
     return (
-      <div>Here is the quote component</div>
+      <div>{getQuoteButton}</div>
     );
   }
 }
